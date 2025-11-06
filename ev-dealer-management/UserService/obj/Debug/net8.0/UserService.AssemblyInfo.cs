ee15060453b68725b9ed04be1522cf8c2621e486
chi-trung <trungnc7062@ut.edu.vn>
2025-11-06 16:33:32 +0700

    sua cong UserServices thanh 5223 dong bo voi fe

--- ev-dealer-management/UserService/obj/Debug/net8.0/UserService.AssemblyInfo.cs
+++ ev-dealer-management/UserService/obj/Debug/net8.0/UserService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7208851c28f13ba7390a9c8c9bdb410384ac1a68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+835962499ff9c3bfc240faf0b123b6513361372c")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
