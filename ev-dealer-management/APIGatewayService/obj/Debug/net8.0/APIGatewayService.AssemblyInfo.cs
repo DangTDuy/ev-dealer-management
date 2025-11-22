6877d1c7272a301c82db8d19f696386bd710c0ff
chi-trung <trungnc7062@ut.edu.vn>
2025-11-22 13:15:31 +0700

    feat: them fields chuc vu(role) cho register

--- ev-dealer-management/APIGatewayService/obj/Debug/net8.0/APIGatewayService.AssemblyInfo.cs
+++ ev-dealer-management/APIGatewayService/obj/Debug/net8.0/APIGatewayService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIGatewayService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1c16602759da0cc029b4c276286d4b650cd4196")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d46fb3fa2c7d1f01c29e10e26676a010a2652c32")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIGatewayService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIGatewayService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ev-dealer-management/VehicleService/obj/Debug/net8.0/VehicleService.AssemblyInfo.cs
+++ ev-dealer-management/VehicleService/obj/Debug/net8.0/VehicleService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VehicleService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1c16602759da0cc029b4c276286d4b650cd4196")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d46fb3fa2c7d1f01c29e10e26676a010a2652c32")]
 [assembly: System.Reflection.AssemblyProductAttribute("VehicleService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VehicleService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
