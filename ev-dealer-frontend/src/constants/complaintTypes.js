355ff01183a283c7ab1f31a77bc822958ef699d9
chi-trung <trungnc7062@ut.edu.vn>
2025-11-26 23:36:37 +0700

    fix status complains

--- ev-dealer-frontend/src/constants/complaintTypes.js
+++ ev-dealer-frontend/src/constants/complaintTypes.js
@@ -1,10 +1,10 @@
 // src/constants/complaintTypes.js
 export const COMPLAINT_TYPES = {
-  GENERAL: "Chung",
-  PRODUCT: "Sản Phẩm",
-  SERVICE: "Dịch vụ",
-  PAYMENT: "Thanh toán",
-  OTHER: "Khác",
+  "Chung": "Chung",
+  "Sản Phẩm": "Sản Phẩm",
+  "Dịch vụ": "Dịch vụ",
+  "Thanh toán": "Thanh toán",
+  "Khác": "Khác",
 };
 
 export const COMPLAINT_TYPE_OPTIONS = Object.values(COMPLAINT_TYPES);
--- ev-dealer-management/SalesService/obj/Debug/net8.0/SalesService.AssemblyInfo.cs
+++ ev-dealer-management/SalesService/obj/Debug/net8.0/SalesService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65d2df68bba7fbf8cdafc4bafecd7fed327ec92e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3061f79022269bf6652b914da7b253a48d03ee2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
