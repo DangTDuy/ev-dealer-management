a09da78e5f8fa549eb00c81c0ba61e94f64bee7c
huynh-dang187 <112720090+huynh-dang187@users.noreply.github.com>
2025-11-27 09:03:24 +0700

    Update Vehicle ( add difference Model Car )

--- ev-dealer-management/NotificationService/obj/Debug/net8.0/NotificationService.AssemblyInfo.cs
+++ ev-dealer-management/NotificationService/obj/Debug/net8.0/NotificationService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotificationService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12e177ac0761c24da5912f1e134b06da3e0dd0c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbd8a68810f76fec13bd91a7e73660ef25b5d6ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotificationService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotificationService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
